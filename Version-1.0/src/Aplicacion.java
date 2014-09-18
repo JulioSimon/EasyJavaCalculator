855613fd07b9a1ddf6168785ec94d1cbd381bc22
Julio José Simón Gil <juliojosesimongil@gmail.com>
2014-09-18 04:23:16 +0200

    Create Aplicacion.java

---
+++ Version-1.0/src/Aplicacion.java
@@ -0,0 +1,31 @@
+package verCalculadoraSimple;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Aplicacion {
+	
+	public static void main (String[] args){
+		
+		// Creamos la Vista
+		Vista panel = new Panel();
+		
+		// Creamos el Modelo
+		Modelo modelo = new Modelo();
+		
+		// Creamos el Controlador y le pasamos la Vista y el Modelo
+		Controlador ctrl = new Controlador(panel,modelo);
+		
+		// Le decimos a la Vista que Controlador va a controlar los eventos.
+		panel.controlador(ctrl);
+		
+		//Creamos el Frame principal de la aplicacion
+		JFrame ventana = new JFrame("Calculadora Simple V1.0 - JJSG");
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana.setContentPane((JPanel)panel);
+		ventana.pack();
+		ventana.setVisible(true);
+		
+	}
+
+}
